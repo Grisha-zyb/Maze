7ced31c86f8fc790658f252e3b70f8be8d55c432
Grisha-zyb <funyandriy@gmail.com>
2025-04-13 18:51:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from pygame import *
 
 WINDOW_SIZE = (1280, 720)
-PLAYER_SIZE = (80,80)
-ENEMY_SIZE = (120, 120)
 
 window = display.set_mode(WINDOW_SIZE)
 background = transform.scale(image.load("fon.png"), WINDOW_SIZE)
@@ -11,6 +9,22 @@ display.set_caption("Лабіринт з ворогом")
 clock = time.Clock()
 FPS = 60
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (80,80))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player = GameSprite("pers.png", 150, 600, 5)
+enemy = GameSprite("zombi.jpg", WINDOW_SIZE[0] - 80, WINDOW_SIZE[1] / 2, 20)
+ex1t = GameSprite("exit.png", 1140, 600, 0)
+
 move = True
 while move:
     for e in event.get():
@@ -18,6 +32,9 @@ while move:
             move = False
 
     window.blit(background, (0, 0))
+    player.draw()
+    ex1t.draw()
+    enemy.draw()
 
     clock.tick(FPS)
     display.update()
